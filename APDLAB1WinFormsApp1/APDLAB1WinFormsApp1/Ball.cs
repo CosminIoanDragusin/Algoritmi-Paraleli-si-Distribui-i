934f40f1232633cfdf8032551478a0d39d8e4b1b
CosminIoanDragusin <121182821+CosminIoanDragusin@users.noreply.github.com>
2023-04-15 15:20:45 +0300

    Add files via upload

---
+++ APDLAB1WinFormsApp1/APDLAB1WinFormsApp1/Ball.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APDLAB1WinFormsApp1
+{
+    class Ball
+    {
+        int px = 0;
+        int py = 0;
+        int size = 0;
+        Color color;
+        Form1 parent = null;
+        Thread bthread = null;
+
+        public Ball(Form1 parent, int px, int py, int size, Color color)
+        {
+            this.parent = parent;
+            this.px = px;
+            this.py = py;
+            this.size = size;
+            this.color = color;
+            bthread = new Thread(new ThreadStart(run));
+            bthread.Start();
+        }
+        public int getPX()
+        {
+            return px;
+        }
+        public int getPY()
+        {
+            return py;
+        }
+        public int getSize()
+        {
+            return size;
+        }
+        public Color getColor()
+        {
+            return color;
+        }
+
+        public void terminateBallThread()
+        {
+         //   bthread.Abort();
+        }
+        public void run()
+        {
+            while (px < parent.Size.Width)
+            {
+                int gravy = 1;
+                int speed = -30;
+                int speedX = 0;
+                int speedY = -30;
+
+                while (true)
+                {
+                    speedY += gravy;
+                    py += speedY;
+                    px += speedX;
+
+                    Thread.Sleep(20);
+                    px++;
+                    parent.Refresh();
+
+                    if (py > parent.Size.Height) 
+                    {
+                        speedY = speed;
+                        speed += 3;
+                    }
+                    if (speed == 0) break;
+                }
+  
+            }
+        }
+    }
+}
---
+++ APDLAB1WinFormsApp1/APDLAB1WinFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APDLAB1WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Ball ball,ball2,ball3 = null;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            this.BackColor = Color.White;
+            ball = new Ball(this, 0, 50, 20, Color.Red);
+            ball2 = new Ball(this, 10, 90, 30, Color.Yellow);
+            ball3 = new Ball(this, 20, 70, 40, Color.Blue);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+                this.Size = new Size(400, 300);
+                this.Name = "Balls";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush brush = new SolidBrush(Color.White);
+            e.Graphics.FillRectangle(brush, 0, 0, Size.Width, Size.Height);
+            brush = new SolidBrush(ball.getColor());
+            e.Graphics.FillEllipse(brush, ball.getPX(), ball.getPY(), ball.getSize(), ball.getSize());
+ 
+            brush = new SolidBrush(ball2.getColor());
+            e.Graphics.FillEllipse(brush, ball2.getPX(), ball2.getPY(), ball2.getSize(), ball2.getSize());
+
+            brush = new SolidBrush(ball3.getColor());
+            e.Graphics.FillEllipse(brush, ball3.getPX(), ball3.getPY(), ball3.getSize(), ball3.getSize());
+            brush.Dispose();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ball.terminateBallThread();
+            ball2.terminateBallThread();
+            ball3.terminateBallThread();
+        }
+    }
+}
